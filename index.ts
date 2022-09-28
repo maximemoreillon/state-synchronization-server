fc962642c4cd6d269ded223f0a7a04a71cca1590
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-28 09:10:19 +0900

    minor

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import http from 'http'
 import express from 'express'
-import { Server } from "socket.io"
 import dotenv from 'dotenv'
+import { Server } from 'socket.io'
 
 dotenv.config()
 
@@ -9,13 +9,11 @@ const {
     PORT = 80
 } = process.env
 
-const socketio_options = {
-    cors: { origin: '*' }
-}
+const cors = { origin: '*' }
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server, socketio_options)
+const io = new Server(server, { cors })
 
 app.get('/', (req, res, next) => {
     res.send('State synchronizer server')
@@ -30,20 +28,18 @@ io.on('connection', (socket) => {
     // Announce connection to other clients
     socket.broadcast.emit('clientConnected', {id})
 
-    socket.on('stateUpdate', (state) => {
-        // The client states have been updated
-        socket.broadcast.emit('clientStateUpdated', { id, state})
-    })
-
-
+    
     socket.on('disconnect', (reason) => {
-
-        // tell other clients that this one has diconnected
-        socket.broadcast.emit('clientDisconnected', {id})
-
         console.log(`Client ${id} disconnected, reason: ${reason}`)
+
+        // Tell other clients that this one has diconnected
+        socket.broadcast.emit('clientDisconnected', {id})
     })
 
+    socket.on('stateUpdate', (state) => {
+        // The client states have been updated, tell others about it
+        socket.broadcast.emit('clientStateUpdated', { id, state })
+    })
 
 })
 
